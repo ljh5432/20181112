0a4925f7f72866528bc7212c91150d6e3ee2b066
지현 이 <ljh5432@gmail.com>
2018-11-12 11:17:51 +0900

    button 여러개 생성하기

--- WindowsFormsApp/WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/WindowsFormsApp/Form1.cs
@@ -18,22 +18,30 @@ namespace WindowsFormsApp
             Load += Form1_Load;
         }
 
+        private Button btn;
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            Button btn = new Button();
-            btn.DialogResult = DialogResult.OK;
+            for(int i=0; i<3; i++)
+            {
+                btn = new Button();
+                btn.DialogResult = DialogResult.OK;
 
-            btn.Text = "확인";
-            btn.Size = new Size(100, 50);
-            btn.Location = new Point(30, 30);
+                btn.Text = string.Format("확인 : {0}", (i+1));
+                btn.Size = new Size(100, 50);
+                btn.Location = new Point((100 * i ) + 30, 30);
+                btn.Cursor = Cursors.Hand;
 
-            Controls.Add(btn);
-            btn.Click += btn_click;
+                Controls.Add(btn);
+                btn.Click += btn_click;
+            }
+            
         }
 
         private void btn_click(object o, EventArgs a)
         {
-            MessageBox.Show("확인하기!");
+            btn = (Button) o;
+            btn.BackColor = (btn.BackColor == Color.Green) ? btn.BackColor = Color.Silver : btn.BackColor = Color.Green;                        
         }
     }
 }
